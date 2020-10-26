018168f62efe130efc0c7e982a2207956460ba54
DESKTOP-JU9ED1F\user <kaushikred293@gmail.com>
2020-10-26 23:50:26 +0530

    Added Registration Form

---
+++ app/src/main/java/com/palmville/rotaract/Registration.java
@@ -0,0 +1,15 @@
+package com.palmville.rotaract;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
